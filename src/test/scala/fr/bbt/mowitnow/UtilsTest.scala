bb46c6f49764c6ff87b7619c3fa6dd536598bae3
bbousque <brice.bousquet@gmail.com>
2017-10-15 10:40:58 +0200

    init du projet

---
+++ src/test/scala/fr/bbt/mowitnow/UtilsTest.scala
@@ -0,0 +1,15 @@
+package fr.bbt.mowitnow
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by brice on 15/10/2017.
+  */
+class UtilsTest extends FunSuite {
+
+  test("Lecture Instructions") {
+//    loadInstructions()
+    assert(true)
+  }
+
+}
